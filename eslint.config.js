2e423794e82b448e2148380b6e7272dfa2a76163
Davlee1 <davidlee21019@gmail.com>
2025-07-22 17:08:23 -0400

    week 1 additions

--- eslint.config.js
+++ eslint.config.js
@@ -1,29 +1,35 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from "@eslint/js";
+import globals from "globals";
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(["dist"]),
   {
-    files: ['**/*.{js,jsx}'],
+    files: ["**/*.{js,jsx}"],
+    settings: { react: { version: "detect" } }, //not sure if this is in the right place
     extends: [
       js.configs.recommended,
-      reactHooks.configs['recommended-latest'],
+      react, // not sure if this is either
+      reactHooks.configs["recommended-latest"],
       reactRefresh.configs.vite,
     ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
       parserOptions: {
-        ecmaVersion: 'latest',
+        ecmaVersion: "latest",
         ecmaFeatures: { jsx: true },
-        sourceType: 'module',
+        sourceType: "module",
       },
     },
     rules: {
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      ...react.configs.recommended.rules,
+      ...react.configs["jsx-runtime"].rules,
+      "no-unused-vars": "warn", //this changes the error to a warning
+      "react/prop-types": "off", //this suppresses warnings about not using
     },
   },
-])
+]);
