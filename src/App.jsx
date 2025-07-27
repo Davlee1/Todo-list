f4ff0281ff607816205a8cbaa066391600a438bd
Davlee1 <davidlee21019@gmail.com>
2025-07-27 16:43:26 -0400

    lesson 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,15 @@
 import "./App.css";
+import "./TodoList.jsx";
+import TodoList from "./TodoList.jsx";
+import TodoForm from "./TodoForm.jsx";
 
 function App() {
-  const todos = [
-    { id: 1, title: "thing 1" },
-    { id: 2, title: "thing b" },
-    { id: 3, title: "objective □" },
-  ];
-
   return (
-    <div>
+    <>
       <h1>My Todos</h1>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.title}</li>
-        ))}
-      </ul>
-    </div>
+      <TodoForm />
+      <TodoList />
+    </>
   );
 }
 
---
+++ src/TodoForm.jsx
@@ -0,0 +1,16 @@
+{/*extract from TodoList.jsx*/}
+const TodoForm = function (){
+    return(
+        <>
+        <form>
+            <label htmlFor="todoTitle">
+            Todo
+            <input type="text" id="todoTitle"/>
+            <button >Add Todo</button>
+            </label>
+        </form>
+        </>
+    )
+}
+
+export default TodoForm
