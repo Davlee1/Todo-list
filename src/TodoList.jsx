bfd3ba2a469d96c1bec02862a4da4fd9a2a378df
Davlee1 <davidlee21019@gmail.com>
2025-08-07 16:26:14 -0400

    week 3 completion

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,22 +1,24 @@
-{/*extract from TodoList.jsx*/}
+{
+  /*extract from TodoList.jsx*/
+}
+import "./TodoListItem.jsx";
+import TodoListItem from "./TodoListItem.jsx";
 
-
-const TodoList = function (){
-    const todos = [
+const TodoList = function () {
+  const todos = [
     { id: 1, title: "thing 1" },
     { id: 2, title: "thing b" },
     { id: 3, title: "objective □" },
   ];
-    return (
+  return (
     <div>
-      
       <ul>
         {todos.map((todo) => (
-          <li key={todo.id}>{todo.title}</li>
+          <TodoListItem key={todo.id} name={todo.title} />
         ))}
       </ul>
     </div>
   );
-}
+};
 
-export default TodoList
+export default TodoList;
