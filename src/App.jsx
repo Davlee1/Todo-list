3ec2235a3ffdf203db5da4ecf1b2f58b21306ff7
Davlee1 <davidlee21019@gmail.com>
2025-09-03 00:08:09 -0400

    week-7 progress

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,17 @@ import TodoForm from "./features/TodoForm.jsx";
 
 function App() {
   const [todoList, setTodoList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+  const url = `https://api.airtable.com/v0/${import.meta.env.VITE_BASE_ID}/${import.meta.env.VITE_TABLE_NAME}`;
+  const token = `Bearer ${import.meta.env.VITE_PAT}`;
+
+  useEffect(() => {
+    const fetchTodos = async () => {
+      setIsLoading(true);
+    };
+    fetchTodos();
+  }, []);
 
   const addTodo = (title) => {
     const newTodo = { title: title, id: Date.now(), isCompleted: false };
@@ -25,19 +36,23 @@ function App() {
   const updateTodo = (editedTodo) => {
     const updatedTodos = todoList.map((x) => {
       if (x.id === editedTodo.id) {
-        return {...editedTodo};
+        return { ...editedTodo };
       }
       return x;
-    })
+    });
     setTodoList(updatedTodos);
-  }
+  };
 
   return (
     <>
       <h1>My Todos</h1>
       <TodoForm onAddTodo={addTodo} />
-      <br/>
-      <TodoList todoList={todoList} onCompleteTodo={completeTodo} onUpdateTodo={updateTodo}/>
+      <br />
+      <TodoList
+        todoList={todoList}
+        onCompleteTodo={completeTodo}
+        onUpdateTodo={updateTodo}
+      />
     </>
   );
 }
